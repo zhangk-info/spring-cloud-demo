198941c152ea668951f4485c072ba32ed93f5f27
ZhangKun <13628344382@139.com>
2021-09-15 10:55:58 +0800

    fix bug :DecryptRequestWrapper 当接收使用对象是(UserInfo(String name;String sex;))并在resolver对对象赋值时会没有对参数名称为name和sex进行赋值（因为只有data）

--- cloud-alibaba-commons/src/main/java/com/zk/encrypted_transfer/DecryptRequestWrapper.java
+++ cloud-alibaba-commons/src/main/java/com/zk/encrypted_transfer/DecryptRequestWrapper.java
@@ -2,7 +2,7 @@ package com.zk.encrypted_transfer;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 密文传输
@@ -40,5 +40,22 @@ public class DecryptRequestWrapper extends HttpServletRequestWrapper {
             return null;
         }
         return value[0];
+    }
+
+    /**
+     * 这个方法重写的目的是：
+     * ?data=encryptStr...的时候只能能达到parameterNames ["data"]
+     * 当接收使用对象是(UserInfo(String name;String sex;))并在resolver对对象赋值时会没有对参数名称为name和sex进行赋值（因为只有data）
+     * 所以将解密的后key同时返回
+     * 注意：此时对象或者参数名称中不应该有data，即任何属性名为data不可用
+     *
+     * @return
+     */
+    @Override
+    public Enumeration<String> getParameterNames() {
+        Set<String> names = new LinkedHashSet<>();
+        names.addAll(Collections.list(super.getParameterNames()));
+        names.addAll(parameterMap.keySet());
+        return Collections.enumeration(names);
     }
 }
