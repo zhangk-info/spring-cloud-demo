73f03b89856cfdff60c5d2f083b86ac44f8560e6
13628344382@139.com <13628344382@139.com>
2022-06-01 14:52:35 +0800

    .

--- cloud-alibaba-commons/src/main/java/com/zk/commons/annotation/feign/OkHttpConfig.java
+++ cloud-alibaba-commons/src/main/java/com/zk/commons/annotation/feign/OkHttpConfig.java
@@ -1,5 +1,6 @@
 //package com.zk.commons.annotation.feign;
 //
+//import feign.Feign;
 //import okhttp3.OkHttpClient;
 //import org.springframework.cloud.commons.httpclient.DefaultOkHttpClientFactory;
 //import org.springframework.context.annotation.Bean;
@@ -17,10 +18,10 @@
 //     * @return
 //     */
 //    @Bean
-//    public feign.okhttp.OkHttpClient client(OkHttpClient okHttpClient) {
+//    public Feign client(OkHttpClient okHttpClient) {
 //        OkHttpClient.Builder builder = okHttpClient.newBuilder();
 //        okhttp3.OkHttpClient httpClient = new DefaultOkHttpClientFactory(builder).createBuilder(true).build();
-//        return new feign.okhttp.OkHttpClient(httpClient);
+//        return Feign.builder().client(new feign.okhttp.OkHttpClient(httpClient)).build();
 //    }
 //
 //}
